dab690916db8ddc511d93ee1099f1b60093465bf
root <advith.nagappa@gmail.com>
2016-11-25 21:34:23 +0100

    namespace learnings

--- consumer-start.sh
+++ consumer-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /home/cep/kafka/kafka_2.10-0.8.2.1/bin
-sh zookeeper-server-start.sh /home/cep/kafka/kafka_2.10-0.8.2.1/config/zookeeper.properties &
-sh kafka-server-start.sh /home/cep/kafka/kafka_2.10-0.8.2.1/config/server.properties &
+cd /usr/src/cep/kafka_2.10-0.8.2.1/bin
+sh zookeeper-server-start.sh /usr/src/cep/kafka_2.10-0.8.2.1/config/zookeeper.properties &
+sh kafka-server-start.sh /usr/src/cep/kafka_2.10-0.8.2.1/config/server.properties &
 sh kafka-topics.sh --create --topic test --zookeeper 10.1.1.1:2181 --partitions 1 --replication-factor 1 &
 sh kafka-console-consumer.sh --topic test --zookeeper 10.1.1.1:2181
  
